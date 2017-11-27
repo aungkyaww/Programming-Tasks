8eeeb1e5a40469b03696d3622629c6b9526a1fe3
AgmasGold <30874283+AgmasGold@users.noreply.github.com>
2017-11-27 22:03:34 +0000

    Create ProjectEuler4.java

---
+++ Project Euler/ProjectEuler4.java
@@ -0,0 +1,34 @@
+public class PE4 {
+
+   
+      
+    public static boolean palindrome(int num) {
+        String number = Integer.toString(num);
+        String[] numArr = new String[number.length()];
+      
+        numArr = number.split("");
+       if (Arrays.equals(numArr, arrayUtils.reverseStringArray(numArr))) {
+         
+         return true;  
+       }
+      
+      return false;
+    }
+    
+    public static void main(String[] args) {
+        int currentNum = 0;
+     
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                int num = j * i;
+               
+                if (palindrome(num) && num > currentNum) {
+                    System.out.print((num + " "));
+                    currentNum = num;
+                }
+            }
+           
+        }
+    }
+
+}
