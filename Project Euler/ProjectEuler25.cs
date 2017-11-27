838ea7ffe3ce000a2ad7834545f8d35aa75492aa
AgmasGold <30874283+AgmasGold@users.noreply.github.com>
2017-11-27 22:39:33 +0000

    Create ProjectEuler25.cs

---
+++ Project Euler/ProjectEuler25.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEuler25
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            BigInteger num1 = new BigInteger(1);
+            BigInteger num2 = new BigInteger(1);
+            BigInteger num3 = new BigInteger(2);
+            int index = 2;
+            while (num3.ToString().Length < 1000)
+            {
+                num3 = BigInteger.Add(num1, num2);
+                num1 = num2;
+                num2 = num3;
+                index++;
+            }
+            Console.WriteLine(index);
+            Console.ReadLine();
+        }
+    }
+}
