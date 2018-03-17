96d567b812cef3dc84b1ac6cf326b178180e382d
JakeYallop <jakezyallop@gmail.com>
2018-03-17 23:55:26 +0000

    Rabbits Iterative Solution

--- Rosalind Solutions/Rabbits and Recurrence Relations/Rabbits and Recurrence Relations/Program.cs
+++ Rosalind Solutions/Rabbits and Recurrence Relations/Rabbits and Recurrence Relations/Program.cs
@@ -11,8 +11,10 @@ namespace Rabbits_and_Recurrence_Relations
 
     class Program
     {
+
+
         //Recursive solution
-        static int CalculateRecurrence(int months, int sizeOfLitter,  int adult, int litter, int currentMonth)
+        static long CalculateRecurrence(int months, int sizeOfLitter, long adult = 0, long litter = 1, int currentMonth = 1)
         {
             if (currentMonth == months)
             {
@@ -20,15 +22,32 @@ namespace Rabbits_and_Recurrence_Relations
             }
             else
             {
-                int newLitter;
-                int newAdults;
+                long newLitter;
+                long newAdults;
                 int newMonth = currentMonth + 1;
                 newAdults = adult + litter;
                 newLitter = adult * sizeOfLitter;
                 return CalculateRecurrence(months, sizeOfLitter, newAdults, newLitter, newMonth);
-            
+
             }
         }
+
+        //Interative/Dynamic solution (Should prevent any out of memory exceptions arising from a recursive solution
+        static long CalculateRecurrenceDynamic(int months, int sizeOfLitter)
+        {
+            //long totalRabbits = 0;
+            long adults = 0;
+            long children = 1;
+
+            for (int i = 0; i < months - 1; i++)
+            {
+                long oldAdults = adults;
+                adults = oldAdults + children;
+                children = oldAdults * sizeOfLitter;
+            }
+            return adults + children;
+        }
+
 
         static void Main(string[] args)
         {
@@ -41,8 +60,16 @@ namespace Rabbits_and_Recurrence_Relations
             }
 
 
-            Console.WriteLine(CalculateRecurrence(5,3,0,1,1));
-            Console.ReadLine();
+            int litter = 0;
+            int months = 0;
+
+            months = int.Parse(lines[0].Substring(0, lines[0].IndexOf(' ')));
+            litter = int.Parse(lines[0].Substring(lines[0].IndexOf(' ')));
+
+            Console.WriteLine(CalculateRecurrence(months, litter, 0));
+            Console.WriteLine(CalculateRecurrence(30, 3));
+            Console.WriteLine(CalculateRecurrenceDynamic(30, 3));
+            Console.ReadLine(); 
         }
     }
 }
