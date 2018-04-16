803b01e675cc7ea7fcf5de1c889be11898cc2f30
JakeYallop <jakezyallop@gmail.com>
2018-04-16 21:29:18 +0100

    Overlap Graph, Enumerating K-mers Lexicographically

---
+++ Rosalind Solutions/EnumeratingKmersLexicographically/EnumeratingKmersLexicographically/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+
+namespace EnumeratingKmersLexicographically
+{
+   
+
+    class Program
+    {
+
+        public static List<string> CalculateSetPermutations(char[] alphabet, int count)
+        {
+            List<string> stringList = new List<string>();
+            CalculateSetPermutationsRecursively(alphabet, "", 0, count, ref stringList);
+            return stringList;
+
+        }
+
+        public static List<string> CalculateSetPermutationsRecursively(char[] alphabet, string str, int pos, int count, ref List<string> stringList)
+        {
+            if (pos == count)
+            {
+                stringList.Add(str);
+                return stringList;
+            }
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+
+                CalculateSetPermutationsRecursively(alphabet, str + alphabet[i], pos + 1, count, ref stringList);
+            }
+            return stringList;
+        }
+
+        static void Main(string[] args)
+        {
+            string strPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string[] files = Directory.GetFiles(strPath + @"\Rosalind", "*.txt"); //Assumes only one txt file in this directory - the data set
+            string[] lines = new string[2];
+            foreach (var file in files)
+            {
+                lines = File.ReadAllLines(file);
+            }
+            char[] symbols = lines[0].Split(' ').ToList().ConvertAll(s => char.Parse(s)).ToArray();
+            int len = int.Parse(lines[1]);
+
+            foreach (string str in CalculateSetPermutations(symbols, len)) 
+            {
+
+                Console.WriteLine(str);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
--- Rosalind Solutions/Overlap Graphs/Overlap Graphs/Program.cs
+++ Rosalind Solutions/Overlap Graphs/Overlap Graphs/Program.cs
@@ -9,6 +9,45 @@ namespace Overlap_Graphs
 
     class Program
     {
+
+        public static List<string> CalculateAdjacencyList((string id, string node)[] nodes, int overlap)
+        {
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            List<string> adjacencyList = new List<string>();
+
+            //build a list of end nodes for each unique end node pattern (e.g AAA, BBB, ACG etc)
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (!dictionary.ContainsKey(nodes[i].node.Substring(0, overlap)))
+                {
+                    dictionary.Add(nodes[i].node.Substring(0, overlap), new List<string>());
+                    dictionary[nodes[i].node.Substring(0, overlap)].Add(nodes[i].id);
+                }
+                else
+                {
+                    dictionary[nodes[i].node.Substring(0, overlap)].Add(nodes[i].id);
+                }
+            }
+
+            //For each list of end nodes in the dictionary, if the end node != start node, add start and end node to adjacency list (edge).
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (dictionary.ContainsKey(nodes[i].node.Substring(nodes[i].node.Length - overlap)))
+                {
+                    foreach (string s in dictionary[nodes[i].node.Substring(nodes[i].node.Length - overlap)])
+                    {
+                        if (nodes[i].id != s)
+                        {
+                            adjacencyList.Add(nodes[i].id.Substring(1) + " " + s.Substring(1));
+                        }
+
+                    }
+                }
+            }
+            return adjacencyList;
+        }
+
+
         static void Main(string[] args)
         {
             string strPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
@@ -19,13 +58,14 @@ namespace Overlap_Graphs
                 lines = File.ReadAllLines(file);
             }
             List<Fasta> fastaList = Fasta.DecodeFasta(lines);
-            string[] dnaStrings = new string[fastaList.Count];
-            for (int i = 0; i < fastaList.Count; i++)
+            (string id, string node)[] nodes = new (string id, string edge)[fastaList.Count] ;
+            for (int i = 0;i < nodes.Length; i++)
             {
-                dnaStrings[i] = fastaList[i].DNA;
+                nodes[i].node = fastaList[i].DNA;
+                nodes[i].id = fastaList[i].ID;
             }
-
-
+            List<string> adjancencyList = CalculateAdjacencyList(nodes, 3);
+            adjancencyList.ForEach(s => Console.WriteLine(s));
             Console.ReadLine();
         }
     }
